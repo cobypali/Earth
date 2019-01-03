189058aa63798efbbe8e60e2a7190483b6cec5a4
cobypali <cobypali@gmail.com>
2019-01-03 08:56:31 -0800

    Version 1.1
    1.1

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Material mat;
+    public Material red;
+    public GameObject pin;
+    public int scene;
+
+    private void OnMouseOver()
+    {
+        pin.GetComponent<Renderer>().material = mat;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        pin.GetComponent<Renderer>().material = red;
+
+    }
+}
---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+    public GameObject go;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider != null)
+            {
+                if (go != hit.collider.gameObject)
+                {
+                    go = hit.transform.gameObject;
+                    Debug.Log("On VR Raycast Enter");
+                }
+            }
+        }
+	}
+}
